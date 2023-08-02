79b8457b4d346a4c265947d2a664a3e456e9b590
Rahul <rahul.rastogi.216000@gmail.com>
2023-08-02 23:15:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors');
+app.use(cors({
+  origin: [
+    // 'http://127.0.0.1:5500',
+  'https://snowboard-finder.netlify.app'
+]
+}));
+app.use(express.json())
+
+app.post("/",async (req,res)=>{
+    let url = req.body.url;
+    let data = await webScraper(url)
+    if(data=="Hmm...We Have 0 Results"){
+
+        res.send(false)
+    }else{
+        res.send(true)
+
+    }
+
+})
+
+app.get("/",async (req,res)=>{
+    res.send("Not Allowed")
+
+})
+
+async function webScraper(url) {
+
+    let text;
+    await axios(url).then(async (response) => {
+        const html_data = await response.data;
+        const $ =  cheerio.load(html_data);
+
+        let ele = $('h1')
+        ele = ele.text()
+        text = ele
+    });
+
+    return text;
+};
+
+
+
+
+
+
+
+
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log("Server is Successfully Running, and App is listening on port " + PORT)
+    } else {
+        console.log('Server not started ' + error);
+    }
+
+});
+
